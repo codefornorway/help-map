985b6b36ceda34f814e86e72dfd861aa9a405a2f
Sefa Bulak <zckhtln@icloud.com>
2025-08-11 22:18:45 +0200

    feat: add user geolocation and accessible search

--- app/components/MapView.vue
+++ app/components/MapView.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-const { locations } = useLocations();
+const { locations, locateMe } = useLocations();
 </script>
 
 <template>
-  <MapboxMap
-    map-id="main-map"
-    class="absolute inset-0 w-full h-full"
-    :options="{
-      center: [8.49, 64.63],
-      zoom: 4.38,
-      style: 'mapbox://styles/mapbox/streets-v11',
-    }">
-    <MapboxDefaultMarker v-for="(location, index) in locations" :key="location.name" :marker-id="`marker-${index}`" :lnglat="location.coordinates" />
-  </MapboxMap>
+  <div class="relative w-full h-full">
+    <button
+      @click="locateMe"
+      class="absolute top-2 right-2 z-10 bg-white border border-gray-300 rounded px-3 py-1 text-sm shadow">
+      Locate Me
+    </button>
+    <MapboxMap
+      map-id="main-map"
+      class="absolute inset-0 w-full h-full"
+      :options="{
+        center: [8.49, 64.63],
+        zoom: 4.38,
+        style: 'mapbox://styles/mapbox/streets-v11',
+      }">
+      <MapboxDefaultMarker v-for="(location, index) in locations" :key="location.name" :marker-id="`marker-${index}`" :lnglat="location.coordinates" />
+    </MapboxMap>
+  </div>
 </template>
