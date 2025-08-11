ad0e72bb3cd19768af3daea93c3d2d6e19459900
Sefa Bulak <zckhtln@icloud.com>
2025-08-11 22:42:17 +0200

    Update MapView.vue

--- app/components/MapView.vue
+++ app/components/MapView.vue
@@ -15,11 +15,7 @@ function iconFor(type: string) {
 
 <template>
   <div class="relative w-full h-full">
-    <button
-      @click="locateMe"
-      class="absolute top-2 right-2 z-10 bg-white border border-gray-300 rounded px-3 py-1 text-sm shadow">
-      Locate Me
-    </button>
+    <button @click="locateMe" class="absolute top-2 right-2 z-10 bg-white border border-gray-300 rounded px-3 py-1 text-sm shadow">Locate Me</button>
     <MapboxMap
       map-id="main-map"
       class="absolute inset-0 w-full h-full"
@@ -28,18 +24,9 @@ function iconFor(type: string) {
         zoom: 4.38,
         style: 'mapbox://styles/mapbox/streets-v11',
       }">
-      <MapboxDefaultMarker
-        v-for="(location, index) in locations"
-        :key="location.name"
-        :marker-id="`marker-${index}`"
-        :lnglat="location.coordinates"
-      >
+      <MapboxDefaultMarker v-for="(location, index) in locations" :key="location.name" :marker-id="`marker-${index}`" :lnglat="location.coordinates">
         <template #marker>
-          <button
-            @click="focus(location)"
-            class="text-xl leading-none"
-            :aria-label="`Select ${location.name}`"
-          >
+          <button @click="focus(location)" class="text-4xl leading-none cursor-pointer" :aria-label="`Select ${location.name}`">
             {{ iconFor(location.type) }}
           </button>
         </template>
